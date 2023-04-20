00e1dbfbb28a5b7965006c5c5eba9e7267777112
zineb essoussi <zineb.zineb.zouzou@gmail.com>
2023-04-20 05:33:59 +0000

    update the carousel && the video style

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,20 @@
 /******************Add the story ******************/
 const image_profile = [
-    ['account1.jpg','zineb'],
-    ['account2.jpg','ikram'],
-    ['account3.jpg','amina'],
-    ['account4.jpg','amal'],
-    ['account5.jpg','amine'],
-    ['account6.jpg','loy'],
-    ['account7.jpg','loy'],
-    ['account8.jpg','loy'],
-    ['account9.jpg','loy'],
-    ['account10.jpg','loy'],
-    ['account11.jpg','loy'],
-    ['account12.jpg','loy'],
-    ['account13.jpg','loy'],
-    ['account14.jpg','loy'],
-    ['account15.jpg','loy'],
+    ['https://i.ibb.co/3S1hjKR/account1.jpg','zineb'],
+    ['https://i.ibb.co/kD6tj9T/account2.jpg','ikram'],
+    ['https://i.ibb.co/cx69NJL/account3-1.jpg','amina'],
+    ['https://i.ibb.co/Zhc5hHp/account4.jpg','amal'],
+    ['https://i.ibb.co/SPTNbJL/account5.jpg','amine'],
+    ['https://i.ibb.co/nj8pPqZ/account6.jpg','loy'],
+    ['https://i.ibb.co/vkXPdxN/account7.jpg','loy'],
+    ['https://i.ibb.co/7R0Vzp3/account8.jpg','loy'],
+    ['https://i.ibb.co/gvrfhjL/account9.jpg','loy'],
+    ['https://i.ibb.co/j8L7FPY/account10.jpg','loy'],
+    ['https://i.ibb.co/JcXRPht/account11.jpg','loy'],
+    ['https://i.ibb.co/6WvdZS9/account12.jpg','loy'],
+    ['https://i.ibb.co/pJ8thst/account13.jpg','loy'],
+    ['https://i.ibb.co/4M3W996/account14.jpg','loy'],
+    ['https://i.ibb.co/Fzpg5yd/account15.jpg','loy'],
 ]
 const story_container = document.querySelector('.owl-carousel.items');
 if(story_container){
@@ -22,7 +22,7 @@ if(story_container){
         const parentDiv = document.createElement('div');
         parentDiv.classList.add("item_s");
         parentDiv.innerHTML = `
-            <img src="./images/${image_profile[i][0]}">
+            <img src="${image_profile[i][0]}">
             <p>${image_profile[i][1]}</p>
             `;
         story_container.appendChild(parentDiv);
@@ -40,17 +40,12 @@ $('.owl-carousel').owlCarousel({
     responsiveClass:true,
     responsive:{
         0:{
-            items:7,
+            items:5,
             nav:true
         },
-        600:{
-            items:8,
+        500:{
+            items:7,
             nav:false
-        },
-        1000:{
-            items:8,
-            nav:true,
-            loop:false
         }
     }
 })
